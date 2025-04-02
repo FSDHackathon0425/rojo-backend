2d6f1469dba72ded0bd4f81e30134aa4c3bbc925
marcorgz1 <marco.valdemoro@gmail.com>
2025-04-02 18:59:58 +0200

    test commit

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 require("dotenv").config();
 
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Conexión exitosa a MongoDB"))
+  .catch((err) => console.error("Error conectando a MongoDB:", err));
+
 /***
  * BOT Commands
  ***/
